53001e2ee070f224faaa2a9aa21e2e84c534e833
hszsz <1360196918@qq.com>
2019-07-20 22:21:34 +0800

    哈哈哈

--- clang/helloworld.c
+++ clang/helloworld.c
@@ -1,9 +1,15 @@
-#include <stdio.h>
-#include <limits.h>
- 
+#include<stdio.h>
 int main()
 {
-   printf("int �洢��С : %lu \n", sizeof(int));
-   
-   return 0;
+    int i,j,result;
+    printf("\n");
+    for (i=1;i<10;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            result=i*j;
+            printf("%d*%d=%-3d",i,j,result); /*-3d表示左对齐，占3位*/
+        }
+    printf("\n"); /*每一行后换行*/
+    }
 }
