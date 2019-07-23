4d9b90425edfe4b25a7864ca5ffbda30d2023897
hszsz <1360196918@qq.com>
2019-07-23 21:34:46 +0800

    国际化i18n

---
+++ java/i18n_3/src/main/java/cn/com/scitc/i18n_3/I18n3Application.java
@@ -0,0 +1,13 @@
+package cn.com.scitc.i18n_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class I18n3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(I18n3Application.class, args);
+    }
+
+}
