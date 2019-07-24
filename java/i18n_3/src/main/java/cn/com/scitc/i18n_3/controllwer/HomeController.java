554ff250800d01a33511c92b3fe476d2548781c0
hszsz <1360196918@qq.com>
2019-07-24 18:08:01 +0800

    frghsdf

--- java/i18n_3/src/main/java/cn/com/scitc/i18n_3/controllwer/HomeController.java
+++ java/i18n_3/src/main/java/cn/com/scitc/i18n_3/controllwer/HomeController.java
@@ -1,4 +1,13 @@
 package cn.com.scitc.i18n_3.controllwer;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
+    @RequestMapping("/")
+    private String page(){
+        return "home";
+    }
 }
